c1f05c8580e1c6923c76e8b9cd7293b634486769
georgeisaac25 <george.isaac25@gmail.com>
2020-01-20 20:08:41 +0530

    added combination of stls and stf

---
+++ lib/StatefulAndStateLessTogether.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var myApp= MaterialApp(
+    home : MyButton(),
+  );
+
+  runApp(myApp);
+}
+
+
+class MyButton extends StatefulWidget{
+  @override
+  MyButtonState createState() {
+    return MyButtonState();
+  }
+}
+
+class MyButtonState extends State<MyButton> {
+  int counter = 0;
+  String defaultCount = "";
+
+  void displayCount(){
+    setState(() {
+      counter++;
+      defaultCount = counter.toString();
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('New entry'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(10),),
+            Text('hi'),
+            Text("Counter clicked : "+ defaultCount,
+              style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,),
+            ),
+            RaisedButton(child: Text('ClickButton'),onPressed: displayCount,)
+          ],
+        ),
+      ),
+    );
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// MyStatelessWidget
+/*class MyStatelessWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        accentColor: Colors.orange,
+      ),
+      title: 'Isaac',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('isaac'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.navigation),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('hi'),
+              Text('hello'),
+              RaisedButton(child: Text('Click'),onPressed: () {},)
+            ],
+          ),
+        ),
+      ),
+      color: Colors.black,
+
+    );
+  }
+
+}
+
+void main() {
+  runApp(MyStatelessWidget());
+}*/
